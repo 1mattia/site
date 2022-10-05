cbf1163f4a92fd9cd22cafa37ee14c45f3a1ca17
mattia203 <mattia.damiana@outlook.it>
2022-10-05 23:10:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from email.mime import image
-from tkinter import Image
 from flask import Flask, render_template, url_for, redirect , request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
